0d96719d81b4001244bc07dc67d8bcef3889187a
PILLAGOLLA DURGA JAYASAI <durgajayasai08072004@gmail.com>
2025-01-26 01:23:26 +0530

    Vercel Navbar Interaction

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <>
+    <div className="relative w-full h-screen flex flex-col items-center justify-between">
+      <h1 className="font-sora text-white text-4xl font-bold mt-20 text-center select-none">
+        Vercel Navbar Interaction
+      </h1>
       <VercelNavbar />
-      <footer className="font-sora text-gray-300 flex gap-1 items-center justify-center mb-10">
+      <footer className="font-sora text-gray-300 flex gap-1 items-center justify-center mb-4 select-none">
         <p>Made with </p>
         <FontAwesomeIcon icon={faHeart} color="#ffffff" />
         <p>by Durgajayasai</p>
       </footer>
-    </>
+    </div>
   );
 }
 
--- src/components/vercel-navbar.jsx
+++ src/components/vercel-navbar.jsx
@@ -4,47 +4,44 @@ import { useState } from "react";
 export function VercelNavbar() {
   const [active, setActive] = useState(items[0]);
   const [isHover, setIsHover] = useState(null);
+
   return (
-    <main className="relative w-full min-h-screen flex items-start md:items-center justify-center px-4 py-10">
-      <div className="relative">
-        <ul className="flex items-center justify-center">
-          {items.map((item) => (
-            <li key={item.id}>
-              <button
-                className="py-2 relative cursor-pointer duration-300 transition-colors hover:!text-white"
-                onClick={() => setActive(item)}
-                onMouseEnter={() => setIsHover(item)}
-                onMouseLeave={() => setIsHover(null)}
-                style={{ color: active.id === item.id ? "#FFF" : "#888888" }}
-              >
-                <div className="px-5 py-2 relative font-sora">
-                  {item.tile}
-                  {isHover?.id === item.id && (
-                    <motion.div
-                      layoutId="hover-bg"
-                      className="absolute bottom-0 left-0 right-0 w-full h-full bg-white/10"
-                      style={{ borderRadius: 6 }}
-                    />
-                  )}
-                </div>
-                {active.id === item.id && (
-                  <motion.div
-                    layoutId="active"
-                    className="absolute bottom-0 left-0 right-0 w-full h-0.5 bg-white"
-                  />
-                )}
-                {isHover?.id === item.id && (
-                  <motion.div
-                    layoutId="hover"
-                    className="absolute bottom-0 left-0 right-0 w-full h-0.5 bg-white"
-                  />
-                )}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </main>
+    <div className="w-full h-full flex items-center justify-center">
+      <ul className="flex flex-wrap md:flex-row items-center justify-center">
+        {items.map((item) => (
+          <li key={item.id} className="relative">
+            <button
+              className="py-2 px-5 font-sora relative cursor-pointer duration-300 transition-colors hover:!text-white"
+              onClick={() => setActive(item)}
+              onMouseEnter={() => setIsHover(item)}
+              onMouseLeave={() => setIsHover(null)}
+              style={{ color: active.id === item.id ? "#FFF" : "#888888" }}
+            >
+              {item.tile}
+              {isHover?.id === item.id && (
+                <motion.div
+                  layoutId="hover-bg"
+                  className="absolute inset-0 bg-white/10"
+                  style={{ borderRadius: 6 }}
+                />
+              )}
+              {active.id === item.id && (
+                <motion.div
+                  layoutId="active"
+                  className="absolute bottom-0 left-0 right-0 w-full h-0.5 bg-white"
+                />
+              )}
+              {isHover?.id === item.id && (
+                <motion.div
+                  layoutId="hover"
+                  className="absolute bottom-0 left-0 right-0 w-full h-0.5 bg-white"
+                />
+              )}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
