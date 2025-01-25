16c198628c175af374771bf11bbcd03e3ce06eee
PILLAGOLLA DURGA JAYASAI <durgajayasai08072004@gmail.com>
2025-01-26 01:24:37 +0530

    Vercel Navbar Interaction

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="relative w-full h-screen flex flex-col items-center justify-between">
-      <h1 className="font-sora text-white text-4xl font-bold mt-20 text-center select-none">
+      <h1 className="font-sora text-white text-xl md:text-4xl font-bold mt-20 text-center select-none">
         Vercel Navbar Interaction
       </h1>
       <VercelNavbar />
-      <footer className="font-sora text-gray-300 flex gap-1 items-center justify-center mb-4 select-none">
+      <footer className="font-sora text-gray-300 flex gap-1 items-center justify-center mb-20 select-none">
         <p>Made with </p>
         <FontAwesomeIcon icon={faHeart} color="#ffffff" />
         <p>by Durgajayasai</p>
